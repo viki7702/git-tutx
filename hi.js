1b06330683f1784c6f310d62a927ddea7011847e
vignesh <vigneshadusumalli2000@gmail.com>
2024-03-21 16:36:55 +0530

    just added file

---
+++ hi.js
@@ -0,0 +1,36 @@
+let radius = 5
+const pi = 3.14
+area = pi * radius * radius
+console.log(area)
+
+num = 2 + 4
+console.log(num)
+
+naistam = "venkat"
+console.log(typeof naistam)
+
+num8 = 7.5
+console.log(typeof num8)
+
+a = 12
+b = 12
+c = a + b + a * b
+console.log(c)
+
+
+author = "a \tppli \vcation \n26snof the "
+console.log(author);
+
+
+
+num1 = 3
+num2 = 4
+result = num1 + num2
+console.log(`the addition of ${x} and ${y} is ${result}`);
+
+let i = 1
+while (i <= 5) {
+    console.log("hi");
+    i++;
+
+}
